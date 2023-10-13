1f9fe14deb3334ccacede2cc717a81028b51d612
Bruno Martins <bgmartins@ucs.br>
2023-10-13 18:48:44 -0300

    feat: :lipstick: Adição de chadCN-ui com alguns componentes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,8 @@
+import { ThemeProvider } from '@/components/theme-provider';
+import '@/styles/globals.css';
 import { Analytics } from '@vercel/analytics/react';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
-import './globals.css';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,7 +17,14 @@ export default function RootLayout({children}: {
   return (
     <html lang="en">
       <body className={inter.className}>
-        {children}
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
         <Analytics/>
       </body>
     </html>
