7602b68b39d03e40fc3d28d165e12dadf191d178
Bruno Martins <bgmartins@ucs.br>
2023-10-14 04:00:52 -0300

    fix: :rotating_light: Ajustando import pra tirar erro de build

--- app/vercel-page/page.tsx
+++ app/vercel-page/page.tsx
@@ -1,5 +1,4 @@
 import { Button } from '@/components/ui/button'
-import { ModeToggle } from '@/components/ui/theme-button'
 import Image from 'next/image'
 
 export default function Home() {
@@ -47,7 +46,6 @@ export default function Home() {
       <Button asChild>
         <a href='/'>get me back to home 🙌</a>
       </Button>
-      <ModeToggle/>
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
         <a
           href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
