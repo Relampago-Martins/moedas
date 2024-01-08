1e11691da9aff9ded759d965bc9d163e89540540
Bruno Martins <bgmartins@ucs.br>
2024-01-07 23:48:46 -0300

    feat: :wrench: update ci

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     env:{
-        API_URL: 'https://8499d504e940f006497084f1d5bad969.balena-devices.com/api'
+        API_URL: 'https://8499d504e940f006497084f1d5bad969.balena-devices.com/api',
+        CI: 'false'
     }
 }
 
