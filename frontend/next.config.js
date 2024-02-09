c04894c46ad0a618f7cc7199c4f44eb4dc94c242
Bruno Martins <bgmartins@ucs.br>
2024-02-08 22:45:38 -0300

    feat: :wrench: Ajustando next config.

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     env:{
-        API_URL: 'https://8499d504e940f006497084f1d5bad969.balena-devices.com/api',
-        CI: 'false'
     }
 }
 
