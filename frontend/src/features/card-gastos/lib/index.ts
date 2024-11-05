2af6b117bbefbfaf036d964f5f72584be0009525
bgmartins <bgmartin@ucs.br>
2024-11-04 21:57:00 -0300

    feat: :poop: Começando nova tela de movimentações.

--- frontend/src/features/card-gastos/lib/index.ts
+++ frontend/src/features/card-gastos/lib/index.ts
@@ -10,3 +10,25 @@ export function calcColor(cor: Categoria['cor'], isActive: boolean) {
     if (isActive) { return cor + 40 }
     return cor;
 }
+
+/**
+ * Recebe uma lista de movimentações e agrupa por data.
+ * grupos de data:
+ * - hoje: movimentações que ocorreram hoje
+ * - ontem: movimentações que ocorreram ontem
+ * - dd/mm: 2 dias atrás
+ * - dd/mm: 3 dias atrás
+ * - dd/mm: 4 dias atrás
+ * - dd/mm: 5 dias atrás
+ * - dd/mm: 6 dias atrás
+ * - dd/mm: mais de 6 dias atrás
+ */
+//export function segmentarPorDatas(movimentacoes: Movimentacao[]) {
+//    const agrupados = movimentacoes.reduce((
+//        
+//    )=>{
+//        return
+//    })
+//
+//    return agrupados;
+//}
