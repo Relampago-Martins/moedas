2956f6d5acc71eaa6296fc1db3d6cc937d1c0717
Bruno Martins <bgmartins@ucs.br>
2025-06-10 23:56:03 -0300

    style(api): 🎨 Ajusta formatação e organização do código
    * Uniformiza o uso de aspas simples e duplas
    * Melhora a legibilidade do código
    * Remove espaços desnecessários

--- frontend/src/shared/api/endpoints/categoria-cli.ts
+++ frontend/src/shared/api/endpoints/categoria-cli.ts
@@ -1,43 +1,46 @@
 'use server';
 
-import { obj2SearchParams } from "@/shared/lib/utils";
-import { TFiltroPeriodo } from "@/types/filters";
-import { Categoria, CategoriaTotalMov } from "@/types/models/categoria";
-import { ApiClient } from "../api-client";
+import { obj2SearchParams } from '@/shared/lib/utils';
+import { TFiltroPeriodo } from '@/types/filters';
+import { Categoria, CategoriaTotalMov } from '@/types/models/categoria';
+import { ApiClient } from '../api-client';
 
-
-export async function getCategorias(tipo?: "D" | "R") {
+export async function getCategorias(tipo?: 'D' | 'R') {
     const urlParams = obj2SearchParams({
         tipo: tipo || undefined,
-        
     });
     const resp = await ApiClient.getInstance().get<Categoria[]>(
-        `/categorias/?${urlParams}`, 
-        { next: { revalidate: 60, tags: ['categorias'] } }
+        `/categorias/?${urlParams}`,
+        { next: { revalidate: 60, tags: ['categorias'] } },
     );
     return resp.data;
 }
 
 type ResumoCategoriasProps = TFiltroPeriodo & {
-    tipo?: "D" | "R";
-    
-}
+    tipo?: 'D' | 'R';
+};
 
 export async function getCategoriasTotalMovs(params: ResumoCategoriasProps) {
     const urlParams = obj2SearchParams(params);
     const resp = await ApiClient.getInstance().get<CategoriaTotalMov[]>(
-        `/categorias/total-movimentacoes/?${urlParams}`, 
+        `/categorias/total-movimentacoes/?${urlParams}`,
     );
-    const total = resp.data.reduce((acc, categoria) => acc + categoria.total_movimentacoes, 0);
-    return resp.data.map(categoria => ({...categoria, percentual: (categoria.total_movimentacoes / total) * 100}));
-
-};
-
-
+    const total = resp.data.reduce(
+        (acc, categoria) => acc + categoria.total_movimentacoes,
+        0,
+    );
+    return resp.data.map((categoria) => ({
+        ...categoria,
+        percentual: (categoria.total_movimentacoes / total) * 100,
+    }));
+}
 
 export async function getCategoria(sigla: string) {
-    const resp = await ApiClient.getInstance().get<Categoria>(`/categorias/${sigla}/`, {
-        next: { revalidate: 300, tags: [`categoria${sigla}`] }
-    });
+    const resp = await ApiClient.getInstance().get<Categoria>(
+        `/categorias/${sigla}/`,
+        {
+            next: { revalidate: 300, tags: [`categoria${sigla}`] },
+        },
+    );
     return resp.data;
 }
