052357812554a51238d3548960259ee095153205
bgmartins <bgmartin@ucs.br>
2025-09-16 21:08:13 -0300

    refactor(estrategias): ♻️ Remove componente CardEstrategiaSkeleton não utilizado

--- frontend/src/app/(user-stuff)/orcamento/estrategias/page.tsx
+++ frontend/src/app/(user-stuff)/orcamento/estrategias/page.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import { getEstrategia } from '@/shared/api/endpoints/estrategia';
-import { Skeleton } from '@/shared/ui/skeleton';
 import { Estrategia } from '@/types/models/estrategia';
 import { useEffect, useState } from 'react';
 
@@ -37,26 +36,6 @@ export default function EstrategiasPage() {
             <p className="text-muted">
                 Aqui você pode visualizar e editar sua estratégia financeira.
             </p>
-        </div>
-    );
-}
-
-function CardEstrategiaSkeleton() {
-    return (
-        <div className="rounded-lg border bg-card text-card-foreground shadow-sm">
-            <div className="flex flex-col space-y-1.5 p-6">
-                <Skeleton className="h-6 w-1/2" />
-                <Skeleton className="h-4 w-3/4" />
-            </div>
-            <div className="space-y-2 p-6 pt-0">
-                <Skeleton className="h-4 w-full" />
-                <Skeleton className="h-4 w-full" />
-                <Skeleton className="h-4 w-full" />
-                <Skeleton className="h-4 w-full" />
-            </div>
-            <div className="flex items-center justify-end p-6 pt-0">
-                <Skeleton className="h-10 w-24" />
-            </div>
         </div>
     );
 }
