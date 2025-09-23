b91adea75872ae0873cd658bc67a9fa25169a9a1
bgmartins <bgmartin@ucs.br>
2025-09-23 20:49:25 -0300

    feat(formas-pagamento): ✨ Refatora o visual  do select de formas de pagamento
    * Cria constante `formasPagamento` com opções de pagamento: Débito, Crédito, Pix e Outros.
    * Atualiza componente `SelectFormaPagamento` para utilizar as novas opções de forma de pagamento.
    * Melhora a interface de seleção de forma de pagamento com botões interativos.

---
+++ frontend/src/entities/movimentacoes/forms/fields/formas-pag.ts
@@ -0,0 +1,22 @@
+export const formasPagamento = [
+    {
+        value: 'deb',
+        iconName: 'ph-wallet',
+        displayName: 'Débito',
+    },
+    {
+        value: 'cre',
+        iconName: 'ph-credit-card',
+        displayName: 'Crédito',
+    },
+    {
+        value: 'pix',
+        iconName: 'ph-pix-logo',
+        displayName: 'Pix',
+    },
+    {
+        value: 'out',
+        iconName: 'ph-coins',
+        displayName: 'Outros',
+    },
+];
--- frontend/src/entities/movimentacoes/forms/fields/select-forma-pagamento.tsx
+++ frontend/src/entities/movimentacoes/forms/fields/select-forma-pagamento.tsx
@@ -1,15 +1,8 @@
 'use client';
-import { getDespesaConfigs } from '@/shared/api/endpoints/despesa-cli';
-import {
-    Select,
-    SelectContent,
-    SelectItem,
-    SelectTrigger,
-    SelectValue,
-} from '@/shared/ui/select';
-import { APIChoice, DespesaSchema } from '@/types/models/despesa';
+import { DespesaSchema } from '@/types/models/despesa';
 import React from 'react';
 import { ControllerRenderProps } from 'react-hook-form';
+import { formasPagamento } from './formas-pag';
 
 type SelectFormaPagamentoProps = ControllerRenderProps<
     DespesaSchema,
@@ -19,31 +12,49 @@ const SelectFormaPagamento = React.forwardRef<
     HTMLSelectElement,
     Omit<SelectFormaPagamentoProps, 'ref'>
 >(({ onChange, value, ...props }, ref) => {
-    const [formasPagamento, setFormasPagamento] = React.useState<APIChoice[]>(
-        [],
-    );
-    React.useEffect(() => {
-        getDespesaConfigs().then((data) => {
-            setFormasPagamento(data.actions.POST.forma_pagamento.choices);
-        });
-    }, []);
-
     return (
-        <Select onValueChange={onChange} defaultValue={value}>
-            <SelectTrigger>
-                <SelectValue placeholder="selecione" ref={ref} {...props} />
-            </SelectTrigger>
-            <SelectContent>
+        <>
+            <div className="grid grid-cols-2 gap-3">
+                {formasPagamento.map((formaPagamento) => {
+                    const active = value === formaPagamento.value;
+
+                    return (
+                        <button
+                            key={formaPagamento.value}
+                            className={`flex items-start justify-between rounded-md border p-3 ${active ? 'border-primary text-primary' : ''}`}
+                            onClick={() => onChange(formaPagamento.value)}
+                            type="button"
+                        >
+                            <div className="flex flex-col items-start gap-2">
+                                <i
+                                    className={`ph ${formaPagamento.iconName} text-xl`}
+                                />
+                                <span className="text-base">
+                                    {formaPagamento.displayName}
+                                </span>
+                            </div>
+                            <div
+                                className={`rounded-full border p-[1px] ${active ? 'border-primary bg-primary' : ''}`}
+                            >
+                                <i
+                                    className={`ph ph-check flex text-xs text-primary-foreground ${active ? '' : 'opacity-0'}`}
+                                />
+                            </div>
+                        </button>
+                    );
+                })}
+            </div>
+            <select value={value} className="hidden">
                 {formasPagamento.map((formaPagamento) => (
-                    <SelectItem
+                    <option
                         key={formaPagamento.value}
                         value={formaPagamento.value}
                     >
-                        {formaPagamento.display_name}
-                    </SelectItem>
+                        {formaPagamento.displayName}
+                    </option>
                 ))}
-            </SelectContent>
-        </Select>
+            </select>
+        </>
     );
 });
 
