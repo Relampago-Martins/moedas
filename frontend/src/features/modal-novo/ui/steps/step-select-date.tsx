6bab4d55da8325de5f8cc7e689e6145f7d166b61
Bruno Gonçalves Martins <bgmartins@ucs.br>
2025-10-09 16:55:56 -0300

    feat(step-select-date): ✨ Adiciona botões rápidos para seleção de data
    * Implementa botões para selecionar "Ontem", "Hoje", "Amanhã", "Próxima Semana" e "Próximo Mês"
    * Refatora a lógica de seleção de data para usar uma função `selectDate`
    * Melhora a estrutura do componente `StepperContent` com um layout mais flexível

--- frontend/src/features/modal-novo/ui/steps/step-select-date.tsx
+++ frontend/src/features/modal-novo/ui/steps/step-select-date.tsx
@@ -1,7 +1,9 @@
 'use client';
 import { StepperContent, useStepper } from '@/entities/stepper/ui/stepper';
+import { Button } from '@/shared/ui/button';
 import { Calendar } from '@/shared/ui/calendar';
-import { useEffect, useState } from 'react';
+import { Separator } from '@/shared/ui/separator';
+import { useCallback, useEffect, useState } from 'react';
 import { DialogOrDrawerHeader } from '../step-header';
 
 export function StepSelectDate() {
@@ -11,23 +13,90 @@ export function StepSelectDate() {
     useEffect(() => {
         events.subscribe('onOpenDateStep', setSelected);
     }, []);
+
+    const selectDate = useCallback(
+        (date: Date | undefined) => {
+            if (!date) return;
+            setSelected(date);
+            events.submit('onSelectDate', date);
+            previous();
+        },
+        [events, previous],
+    );
+
     return (
-        <StepperContent value="calendario" level={2} className="md:w-[25rem]">
+        <StepperContent value="calendario" level={2} className="md:w-fit">
             <DialogOrDrawerHeader
                 title={'Data da Compra'}
                 onBack={() => previous()}
             />
-            <Calendar
-                mode="single"
-                selected={selected}
-                onSelect={(date) => {
-                    if (!date) return;
-                    setSelected(date);
-                    events.submit('onSelectDate', date);
-                    previous();
-                }}
-                className="w-full"
-            />
+            <div className="flex gap-2">
+                <div className="flex flex-col gap-2">
+                    <Button
+                        variant={'ghost'}
+                        size={'sm'}
+                        className="justify-start"
+                        onClick={() => {
+                            const yesterday = new Date();
+                            yesterday.setDate(yesterday.getDate() - 1);
+                            selectDate(yesterday);
+                        }}
+                    >
+                        Ontem
+                    </Button>
+                    <Button
+                        variant="ghost"
+                        size={'sm'}
+                        className="justify-start"
+                        onClick={() => selectDate(new Date())}
+                    >
+                        Hoje
+                    </Button>
+                    <Button
+                        variant="ghost"
+                        size={'sm'}
+                        className="justify-start"
+                        onClick={() => {
+                            const tomorrow = new Date();
+                            tomorrow.setDate(tomorrow.getDate() + 1);
+                            selectDate(tomorrow);
+                        }}
+                    >
+                        Amanhã
+                    </Button>
+                    <Button
+                        variant="ghost"
+                        className="h-auto justify-start text-nowrap py-1"
+                        size={'sm'}
+                        onClick={() => {
+                            const nextWeek = new Date();
+                            nextWeek.setDate(nextWeek.getDate() + 7);
+                            selectDate(nextWeek);
+                        }}
+                    >
+                        Próxima Semana
+                    </Button>
+                    <Button
+                        variant="ghost"
+                        className="h-auto justify-start text-nowrap py-1"
+                        size={'sm'}
+                        onClick={() => {
+                            const nextMonth = new Date();
+                            nextMonth.setMonth(nextMonth.getMonth() + 1);
+                            selectDate(nextMonth);
+                        }}
+                    >
+                        Próximo Mês
+                    </Button>
+                </div>
+                <Separator orientation="vertical" className="" />
+                <Calendar
+                    mode="single"
+                    selected={selected}
+                    onSelect={selectDate}
+                    className="w-[300px]"
+                />
+            </div>
         </StepperContent>
     );
 }
